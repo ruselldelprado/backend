ac0e4cd24512c16d02c48af1238f220e0c34941f
Rusell Del Prado <rusellvdelprado@gmail.com>
2025-03-27 19:05:38 +0800

    redirect to login page when user access urls and not logged in

--- backend/dispatch/views.py
+++ backend/dispatch/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render,redirect
 from .models import Incident
 from .forms import IncidentForm
+from django.contrib.auth.decorators import login_required
+
 
 def hero(request):
     return render(request, 'dispatch/hero.html')
 
+
+@login_required(login_url='/members/login_user/')
 def home(request):
     return render(request, 'dispatch/home.html')
 
+
+@login_required(login_url='/members/login_user/')
 def personel(request):
     return render(request, 'dispatch/personel.html')
 
 
+@login_required(login_url='/members/login_user/')
 def dispatcher_report(request):
     return render(request, 'dispatch/dispatcher_report.html')
 
+
+@login_required(login_url='/members/login_user/')
 def showView(request):
     obj = Incident.objects.all()
     template_name = 'dispatch/calls.html'
     context = {'calls': obj}
     return render(request, template_name, context)
 
+
+@login_required(login_url='/members/login_user/')
 def callsFormView(request):
     form = IncidentForm()
     if request.method == 'POST':
@@ -32,6 +43,8 @@ def callsFormView(request):
     context = {'form': form}
     return render(request, template_name, context)
 
+
+@login_required(login_url='/members/login_user/')
 def updateView(request, id):
     obj = Incident.objects.get(id=id)
     form = IncidentForm(instance=obj)
@@ -45,6 +58,7 @@ def updateView(request, id):
     return render(request, template_name, context)
 
 
+@login_required(login_url='/members/login_user/')
 def deleteView(request, id):
     obj = Incident.objects.get(id=id)
     if request.method == 'POST':
